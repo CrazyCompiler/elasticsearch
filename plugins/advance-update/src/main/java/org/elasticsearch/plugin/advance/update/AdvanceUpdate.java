9519111a48594c1c77dd40216816e9de0638df93
Viraj Parab <virajpa@thoughtworks.com>
2017-12-18 21:53:59 +0530

    <PAM-000> [Viraj] - Add Seperate _advanceupdate plugin which points to core update

---
+++ plugins/advance-update/src/main/java/org/elasticsearch/plugin/advance/update/AdvanceUpdate.java
@@ -0,0 +1,35 @@
+package org.elasticsearch.plugin.advance.update;
+
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class AdvanceUpdate extends Plugin implements ActionPlugin {
+
+    @Override
+    public List<RestHandler> getRestHandlers(Settings settings,
+                                             RestController restController,
+                                             ClusterSettings clusterSettings,
+                                             IndexScopedSettings indexScopedSettings,
+                                             SettingsFilter settingsFilter,
+                                             IndexNameExpressionResolver indexNameExpressionResolver,
+                                             Supplier<DiscoveryNodes> nodesInCluster) {
+        Handler handler = new Handler(settings, restController);
+        List<RestHandler> listHandlers = new ArrayList<>();
+        listHandlers.add(handler);
+        return listHandlers;
+    }
+
+}
